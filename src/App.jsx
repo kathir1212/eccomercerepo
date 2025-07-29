cf08f7030098a48b2eff52ccd069c270efba173d
kathir1212 <kathirvel082@gmail.com>
2025-07-29 18:09:33 +0530

    add deploy link

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <>
       {showUserLogin && <Login />}
-      {!isSeller && <Navbar />}
+       <Navbar />
 
       <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
         <Routes>
